2074d908d87097a6ec80579d04ec60c199f4b66f
Aditya Raval <adi7017@gmail.com>
2019-08-16 11:51:54 +0530

    Updates

---
+++ precache-manifest.5853af4645cd695496daa0f53fa1f61f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e94982def83ceb788eee03b3db625a1",
+    "url": "/monsters-rolodx/index.html"
+  },
+  {
+    "revision": "5f453a6bfa9063f7bb62",
+    "url": "/monsters-rolodx/static/css/main.5b46ee1e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodx/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5f453a6bfa9063f7bb62",
+    "url": "/monsters-rolodx/static/js/main.419e598b.chunk.js"
+  },
+  {
+    "revision": "2ea83a86ceb04362959d",
+    "url": "/monsters-rolodx/static/js/runtime~main.f0b6157a.js"
+  }
+]);
